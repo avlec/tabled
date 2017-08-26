bd47a7bac5fda8fca86a6b7f1b9215c1e596927c
Alec Cox <daqx1444@gmail.com>
2017-08-26 09:13:04 -0700

    fixing

--- main/apps.py
+++ main/apps.py
@@ -5,9 +5,3 @@ class MainConfig(AppConfig):
     name = 'tabled'
     label = 'tabled'
     verbose_name = 'Tabled'
-
-    def ready(self):
-        Customer = self.get_model('Customer')
-        Reservation = self.get_model('Reservation')
-        Table = self.get_model('Table')
-        Restaurant = self.get_model('Restaurant')
