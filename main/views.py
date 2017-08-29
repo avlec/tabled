9329253e09b31db2540a070e5daa74dca9cd8288
Alec Cox <daqx1444@gmail.com>
2017-08-29 10:08:30 -0700

    working on views

--- main/views.py
+++ main/views.py
@@ -7,26 +7,43 @@ from .models import *
 
 def index(request):
     context = {
-        'restaurant_list' : Restaurant.objects.all()
+        'restaurant_list': Restaurant.objects.all(),
+        'title': 'Get a Table',
+        'current_page': 'customer.html',
     }
     return render(request, 'index.html', context)
 
 
 def customer(request, customer_id):
     Customer.objects.get(id=customer_id)
-    return HttpResponse('customer' + customer_id)
-
+    context = {
+        'title': 'Get a Table',
+        'current_page': 'customer.html',
+    }
+    return render(request, 'customer.html', context)
 
 def reservation(request, reservation_id):
     Reservation.objects.get(id=reservation_id)
-    return HttpResponse('reservation' + reservation_id)
+    context = {
+        'title': 'Get a Table',
+        'current_page': 'customer.html',
+    }
+    return render(request, 'reservation.html', context)
 
 
 def table(request, table_id):
     Table.objects.get(id=table_id)
-    return HttpResponse('table' + table_id)
+    context = {
+        'title': 'Get a Table',
+        'current_page': 'customer.html',
+    }
+    return render(request, 'table.html', context)
 
 
 def restaurant(request, restaurant_id):
-    Restaurant.objects.get(id=restaurant_id)
-    return HttpResponse('restaurant' + restaurant_id)
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    context = {
+        'title' : restaurant.restaurant_name,
+        'current_page': 'customer.html',
+    }
+    return render(request, 'restaurant.html', context)
